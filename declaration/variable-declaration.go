e88fa57596e2334c56c249d52bd258027a673e88
Gentjan Kolicaj <gentjankolicaj@gmail.com>
2021-01-09 12:07:52 +0100

    More variable declaration examples.

--- declaration/variable-declaration.go
+++ declaration/variable-declaration.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
-var a, b int = 9, 10
+var a, b = 9, 10
 
 var (
-	A, B int = 11, 12
+	A, B = 11, 12
 )
 
 func main() {
-	fmt.Println(a, b)
-	fmt.Println(A, B)
+	fmt.Println(a, b, A, B)
+
+	var c, d = 13, 14
+	var e, f = 17, 18
+
+	var (
+		C = 15
+		D = 16
+
+		E = 19
+		F = 20
+	)
+
+	fmt.Println(c, d, C, D)
+	fmt.Println(e, f, E, F)
 }
